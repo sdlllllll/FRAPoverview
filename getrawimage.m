469a3273dc38058999dad2a7f6947ddab823fda9
sdlllllll <sdlllllll3@gmail.com>
2019-04-22 16:19:13 +0800

    getrawimage.m: addcomments and revise disp() to fprintf()

--- getrawimage.m
+++ getrawimage.m
@@ -1,5 +1,16 @@
-function [roiimage, imgs, frame] = getrawimage(date, experiment)
-%%
+function [imgs, roiimage] = getrawimage(date,experiment)
+% Input date and experiment name, return original images and ROI image.
+% 
+% [imgs, roiimage] = getrawimage(date,experiment)
+%     originalimgs(m,n,t,c):
+%         m: high
+%         n: width
+%         t: time
+%         c: channel
+%     roiimage(m,n):
+%         m: high
+%         n: width
+
 roiimage = imread(sprintf('%s/%s_ROI.tif', date, experiment));
 [x, y] = size(roiimage);
 imgdir = sprintf('%s/%s', date, experiment);
@@ -7,6 +18,7 @@ list = dir(imgdir);
 filenum = size(list, 1);
 maxframe = 0;
 maxchannel = 0;
+% Test filename length.
 for k=1:filenum
     try
         t = extractAfter(string(regexp(list(k).name, '_t[0-9]+',...
@@ -28,6 +40,7 @@ for k=1:filenum
 end
 frame = maxframe;
 imgs = zeros(x, y, frame, maxchannel);
+% Put original images into matrix.
 for c=1:maxchannel
     for t=1:frame
         switch lent
@@ -42,9 +55,10 @@ for c=1:maxchannel
         end
     end
 end
-%%
 mkdir(sprintf('result/%s/%s', date, experiment))
+% Save data in .mat file with -v7.3.
 save(sprintf('result/%s/%s/data.mat', date, experiment),...
     'imgs', 'roiimage', '-v7.3', '-nocompression');
-disp(sprintf('Get original image finished.'));
+fprintf('Get original image finished.\n');
+return
 end
