a881cb423832fa961b3f8461442e31c5bd0f8771
sdlllllll <sdlllllll3@gmail.com>
2019-04-22 17:32:04 +0800

    revise getrawfrap.m format and add comment

--- getrawfrap.m
+++ getrawfrap.m
@@ -1,15 +1,18 @@
-function [rawfrapinf] = getrawfrap(imgs, roiMatrix, channel, date, experiment)
-%%
-t = size(imgs, 3);
-n = size(roiMatrix, 3);
-rawfrapinf = zeros(t, n);
+function [rawfrapinf] = getrawfrap(imgs,roiMatrix,channel,date,experiment)
+% Sum pixel brightness in each ROIs every fraps.
+%
+% [rawfrapinf] = getrawfrap(imgs,roiMatrix,channel,date,experiment)
+
+t = size(imgs,3);
+n = size(roiMatrix,3);
+rawfrapinf = zeros(t,n);
 for u = 1:t
     for m = 1:n
-        r = imgs(:, :, u, channel).*roiMatrix(:, :, m);
-        rawfrapinf(u, m) = sum(sum(r));
+        r = imgs(:,:,u,channel).*roiMatrix(:,:,m);
+        rawfrapinf(u,m) = sum(sum(r));
     end
 end
-save(sprintf('result/%s/%s/data.mat', date, experiment), 'rawfrapinf',...
-    '-append', '-nocompression');
-disp('Statistic FRAP finished.');
+save(sprintf('result/%s/%s/data.mat',date,experiment),'rawfrapinf',...
+    '-append','-nocompression');
+fprintf('Statistic FRAP finished./n');
 end
