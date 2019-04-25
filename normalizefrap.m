833e572c71ee35566b22cd06843de98510354cfe
sdlllllll <sdlllllll3@gmail.com>
2019-04-25 09:22:36 +0800

    add notes in comment in normalizefrap.m

--- normalizefrap.m
+++ normalizefrap.m
@@ -1,6 +1,8 @@
 function [normalizedfrapinf] = normalizefrap(rawfrapinf,date,experiment)
 % Normalize FRAP statistic results.
 
+% Sometimes the lowest value is not at the frame after bleaching, how to
+% notmalize data at this case?
 normalizedfrapinf = normalize(rawfrapinf,'range');
 save(sprintf('result/%s/%s/data.mat',date,experiment),...
     'normalizedfrapinf','-append','-nocompression');
