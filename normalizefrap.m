a9a43ff37cf02928f6a282ce8a0bf0e829f01474
sdlllllll <sdlllllll3@gmail.com>
2019-04-25 09:10:46 +0800

    add comment in normalizefrap.m

--- normalizefrap.m
+++ normalizefrap.m
@@ -1,7 +1,8 @@
-function [normalizedfrapinf] = normalizefrap(rawfrapinf, date, experiment)
-%%
-normalizedfrapinf = normalize(rawfrapinf, 'range');
-save(sprintf('result/%s/%s/data.mat', date, experiment),...
-    'normalizedfrapinf', '-append', '-nocompression');
-disp('FRAP information normalized.')
+function [normalizedfrapinf] = normalizefrap(rawfrapinf,date,experiment)
+% Normalize FRAP statistic results.
+
+normalizedfrapinf = normalize(rawfrapinf,'range');
+save(sprintf('result/%s/%s/data.mat',date,experiment),...
+    'normalizedfrapinf','-append','-nocompression');
+fprintf('FRAP information normalized.\n')
 end
